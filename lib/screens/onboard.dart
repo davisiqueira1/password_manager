09b7d01c508635d579c925d87bce6e876bb348cf
Davi Siqueira <davisvsiqueira@gmail.com>
2024-06-26 23:13:15 -0300

    feat: integrate widgets on OnboardScreen

--- lib/screens/onboard.dart
+++ lib/screens/onboard.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:password_manager/utils/color_palette.dart';
 import 'package:password_manager/widgets/button_to_login.dart';
+import 'package:password_manager/widgets/carousel_onboard.dart';
 import 'package:password_manager/widgets/register_button.dart';
 
 class OnboardScreen extends StatelessWidget {
@@ -9,25 +8,19 @@ class OnboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: RegisterButton(
-                screenWidth: screenWidth,
-                onPressed: () {},
-              ),
-            ),
-            Center(
-              child: ButtonToLogin(
-                screenWidth: screenWidth,
-                onPressed: () {},
-              ),
-            ),
-          ],
+        body: Container(
+          margin: EdgeInsets.symmetric(vertical: screenHeight * 0.015),
+          child: Column(
+            children: [
+              const CarouselOnboard(),
+              RegisterButton(onPressed: () {}),
+              SizedBox(height: screenHeight * 0.015),
+              ButtonToLogin(onPressed: () {}),
+            ],
+          ),
         ),
       ),
     );
