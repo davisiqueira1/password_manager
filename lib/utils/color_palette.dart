63d9d826134e5aa80bba1eda186ea8bb52b66ce1
Davi Siqueira <davisvsiqueira@gmail.com>
2024-06-26 23:10:36 -0300

    feat: add grey to color palette

--- lib/utils/color_palette.dart
+++ lib/utils/color_palette.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class ColorPalette {
   static const black = Color(0xFF292D32);
   static const white = Color(0xFFFFFFFF);
+  static const grey = Color(0xFF878787);
 }
