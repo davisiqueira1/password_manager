a498b965ee449641fb6b2f02cad02a5422843da7
Davi Siqueira <davisvsiqueira@gmail.com>
2024-06-28 21:01:06 -0300

    feat: add labelGrey and error colors to color palette

--- lib/utils/color_palette.dart
+++ lib/utils/color_palette.dart
@@ -4,4 +4,6 @@ class ColorPalette {
   static const black = Color(0xFF292D32);
   static const white = Color(0xFFFFFFFF);
   static const grey = Color(0xFF878787);
+  static const labelGrey = Color(0xFF9B9B9B);
+  static const error = Color.fromARGB(255, 255, 190, 190);
 }
